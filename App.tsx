a1a758215dc6ca8ebdac72898d20d428f73ad97a
Dan Forest <dforest@meddbase.com>
2025-02-24 11:06:44 +0000

    Fix to use single quotes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { View, Text, Button, FlatList } from "react-native";
-import { NativeModules } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList } from 'react-native';
+import { NativeModules } from 'react-native';
 
 const { FileScannerModule } = NativeModules;
 
@@ -12,24 +12,24 @@ const App = () => {
             const result: FileInfo[] = await FileScannerModule.ScanFiles();
             setFiles(result);
         } catch (error) {
-            console.error("Error scanning files:", error);
+            console.error('Error scanning files:', error);
         }
     };
 
     return (
         <View style={{ padding: 20 }}>
-            <Button title="Scan files" onPress={scanFiles} />
+            <Button title='Scan files' onPress={scanFiles} />
 
             <FlatList
                 data={files}
                 contentContainerStyle={{ padding: 3 }} 
                 keyExtractor={(item) => item.Id}
                 renderItem={({ item }) => (
-                    <View style={{ marginVertical: 10, padding: 10, backgroundColor: "#8B4513", borderRadius: 5 }}>
-                        <Text style={{ color: "white" }}>Artist: {item.Artist}</Text>
-                        <Text style={{ color: "white" }}>Title: {item.Name}</Text>
-                        <Text style={{ color: "white" }}>Album: {item.Album}</Text>
-                        <Text style={{ color: "white" }}>Path: {item.Path}</Text>
+                    <View style={{ marginVertical: 10, padding: 10, backgroundColor: '#8B4513', borderRadius: 5 }}>
+                        <Text style={{ color: 'white' }}>Artist: {item.Artist}</Text>
+                        <Text style={{ color: 'white' }}>Title: {item.Name}</Text>
+                        <Text style={{ color: 'white' }}>Album: {item.Album}</Text>
+                        <Text style={{ color: 'white' }}>Path: {item.Path}</Text>
                     </View>
                 )}
             />
