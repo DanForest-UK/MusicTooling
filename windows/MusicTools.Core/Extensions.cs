6230c6801f9e833d5fb1d5cc690afe5e14596d47
Dan Forest <dforest@meddbase.com>
2025-02-27 10:38:23 +0000

    Update to allow nullable references Extensions updated

--- windows/MusicTools.Core/Extensions.cs
+++ windows/MusicTools.Core/Extensions.cs
@@ -8,11 +8,12 @@ namespace MusicTools.Core
 {
     public static class Extensions
     {
-        public static Option<string> ValueOrNone(this string value) =>
-            value.HasValue()
-                ? Some(value)
-                : None;
+        public static Option<string> ValueOrNone(this string? value) =>
+        value.HasValue()
+            ? Some(value!)
+            : None;
 
-        public static bool HasValue(this string value) => !string.IsNullOrWhiteSpace(value);
+        public static bool HasValue(this string? value) =>
+            !string.IsNullOrWhiteSpace(value);
     }
 }
