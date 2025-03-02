c49b3895da71932da7253d69abc001701bf78482
Dan Forest <dforest@meddbase.com>
2025-03-02 01:44:17 +0000

    Firewall exception for localhost port for return authentication from Spotify

--- windows/MusicTools/App.xaml.cs
+++ windows/MusicTools/App.xaml.cs
@@ -2,6 +2,8 @@ using Microsoft.ReactNative;
 using MusicTools;
 using MusicTools.Logic;
 using MusicTools.NativeModules;
+using System.Diagnostics;
+using System;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -12,6 +14,8 @@ namespace MusicTools
     {
         public App()
         {
+            AddFirewallException();
+
             // Dependency injection
             Runtime.GetFilesWithExtensionAsync = React.GetFilesWithExtensionAsync;
             Runtime.ReadSongInfo = ReadTag.ReadSongInfo;
@@ -40,6 +44,34 @@ namespace MusicTools
             InitializeComponent();
         }
 
+ 
+
+            private void AddFirewallException()
+            {
+                try
+                {
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "netsh",
+                        Arguments = "advfirewall firewall add rule name=\"Spotify Auth Callback\" dir=in action=allow protocol=TCP localport=8888",
+                        Verb = "runas",
+                        CreateNoWindow = true,
+                        UseShellExecute = false
+                    };
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    process.WaitForExit();
+                    Debug.WriteLine("Added firewall rule for port 8888");
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Failed to add firewall rule: {ex.Message}");
+                    // Continue anyway, as the rule might already exist or we don't have sufficient privileges
+                }
+            }
+
+     
         /// <summary>
         /// Entry point
         /// </summary>
